100a87b4b293d6102218a177603fd32dad861b18
anaj00 <bantolino.jana@gmail.com>
2025-02-09 21:29:33 +0800

    feat: created multiple file feature

--- fix_header.py
+++ fix_header.py
@@ -3,14 +3,15 @@
 import os
 import pathlib
 import csv
+from colorama import Fore, Style, init
 
+init(autoreset=True)
 
-def main():
-    folder_name = input("Enter folder to convert: ")
-
-    # C:\CPCe_41_inst\OMLC_code-7CAT.txt
-    # C:\CPCe_41_inst\NACRE_SHINE_40 _TUBB.txt
-    codefile_path = input("Enter full path to codefile: ")
+def process_folder(folder_name, codefile_path):
+    # folder_name = input("Enter folder to convert: ")
+    # # C:\CPCe_41_inst\OMLC_code-7CAT.txt
+    # # C:\CPCe_41_inst\NACRE_SHINE_40 _TUBB.txt
+    # codefile_path = input("Enter full path to codefile: ")
     cwd = os.getcwd()
 
     for (dirpath, _, filenames) in os.walk(folder_name):
@@ -35,7 +36,22 @@ def main():
             with open(full_cpc_filename, "w") as f:
                f.write(",".join(header) + "\n" + remaining)
 
-    print("Conversion successful")
+    print("Conversion successful for folder: " + folder_name + "\n")
+    
+def main():
+    print("\nReading input_file.txt...\n")
+    with open("input_file.txt", "r", encoding="utf-8") as file:
+        reader = csv.reader(file)
+        next(reader) # skip header
+        
+        for row in reader:
+            codefile_path = row[0].strip().strip('"')
+            folder_name = row[1].strip().strip('"')
+            
+            print(f"Processing folder: {Fore.GREEN}<{folder_name}>{Style.RESET_ALL} with code file: {Fore.GREEN}<{codefile_path}>{Style.RESET_ALL}")
+            process_folder(folder_name, codefile_path)
+            
+    print("\nProcessing done.")
 
 
 if __name__ == '__main__':
